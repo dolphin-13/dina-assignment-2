859ccae666cb31e1158c431eac2fd915f6055b0f
dolphin-13 <55006453+dolphin-13@users.noreply.github.com>
2021-09-25 23:54:27 -0700

    Update for Assignment2

--- src/com/codercampus/Assignment2Application.java
+++ src/com/codercampus/Assignment2Application.java
@@ -7,29 +7,30 @@ public class Assignment2Application {
 
 	public static void main(String[] args) {
 		int userInput = 0;
-		int shot = 1;
+		int shot = 0;
 		int number = randomNumber();
 
 		Scanner s = new Scanner(System.in);
 
-		for (int i = 0; i < 5; i++) {
+		while (shot < 6) {
 			System.out.print("Pick a number between 1 and 100 ");
-			if (s.hasNextInt()) {
-				userInput = s.nextInt();
-				if (userInput == number) {
-					System.out.println("You Win!");
-					break;
-				} else if (userInput < number) {
-					System.out.println("Please pick a higher number ");
-				} else {
-					System.out.println("Please pick a lower number ");
-				}
-				if (shot == 5) {
-					System.out.println("You lose!");
-					System.out.println("The number to guess was: " + number);
-					break;
-				}
+			userInput = s.nextInt();
+			if (userInput < 1 || userInput > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again ");
+			} else if (userInput == number) {
+				System.out.println("You Win!");
+				break;
+			} else if (userInput < number) {
+				System.out.println("Please pick a higher number ");
 				shot++;
+			} else {
+				System.out.println("Please pick a lower number ");
+				shot++;
+			}
+			if (shot == 5) {
+				System.out.println("You lose!");
+				System.out.println("The number to guess was: " + number);
+				break;
 			}
 		}
 		s.close();
